706102a3cc9a09d8cb886834f1e89b1422eae564
ericchu1329 <kiyuen88ec@gmail.com>
2022-02-28 22:25:41 -0800

    Implemented Endless Platform Generation
    - PlatformSpawner contains endless platform generation algorithm
    - also added background-model-01 along with its textures

---
+++ Endless-Geo-Dash/Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,38 @@
+/*
+NAME: GPEJ
+MEMBERS: Eric Chu, Jake Wong
+COURSE: CPSC 254-01
+
+FILE DESCRIPTION:
+This file contains the PlatformSpawner class, which will spawn new platforms when the player has 
+left the previous platform.
+*/
+
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] lanes;
+
+    [SerializeField] private float lane_length;
+
+    private bool is_triggered = true;
+
+    private void OnTriggerEnter()
+    {
+        if (is_triggered)
+        {
+            Vector3 spawn_point = new Vector3(transform.position.x, transform.position.y, transform.position.z + lane_length);
+            Quaternion orientation = Quaternion.Euler(Vector3.zero);
+            int lane_index = Random.Range(0, 5);
+            Instantiate(lanes[lane_index], spawn_point, orientation);
+
+            is_triggered = false;   // prevents double triggers
+        }
+    }
+
+    private void OnTriggerExit()
+    {
+        Destroy(transform.parent.gameObject, 2f);    
+    }
+}
