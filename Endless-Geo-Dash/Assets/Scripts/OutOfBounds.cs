3d2bef5c9fd5a7e711cfcd5955b291957c94d269
ericchu1329 <kiyuen88ec@gmail.com>
2022-03-09 19:31:13 -0800

    Added three new platforms
    TODO for test-scene:
    - add 9 more platform variants: 3 for platform left, 3 for platform mid, and 3 for platform right
    - add ui button sound to the buttons

---
+++ Endless-Geo-Dash/Assets/Scripts/OutOfBounds.cs
@@ -0,0 +1,18 @@
+/*
+NAME: GPEJ
+MEMBERS: Eric Chu, Jake Wong
+COURSE: CPSC 254-01
+
+FILE DESCRIPTION:
+This file contains the OutOfBounds class, which kills the player when
+they collide with an OutOfBounds object
+*/
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    private void OnTriggerEnter()
+    {
+        GameStateManager.instance.TransitionToLostState();   
+    }
+}
