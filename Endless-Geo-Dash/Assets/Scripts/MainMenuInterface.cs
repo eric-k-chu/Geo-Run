f4e5e130284e0c868dd24b47b0afa579525e2262
ericchu1329 <kiyuen88ec@gmail.com>
2022-03-12 02:08:59 -0800

    added menu animations along with game title

--- Endless-Geo-Dash/Assets/Scripts/MainMenuInterface.cs
+++ Endless-Geo-Dash/Assets/Scripts/MainMenuInterface.cs
@@ -13,6 +13,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuInterface : MonoBehaviour
 {
+    [SerializeField] private Animator logo_ac;
+
+    [SerializeField] private Animator play_ac;
+
+    [SerializeField] private Animator settings_ac;
+
+    [SerializeField] private Animator quit_ac;
+
+    [SerializeField] private GameObject menu_options_background;
+
+    private void Start()
+    {
+        play_ac.gameObject.SetActive(false);
+        settings_ac.gameObject.SetActive(false);
+        quit_ac.gameObject.SetActive(false);
+        menu_options_background.SetActive(false);
+    }
+
     public void LoadGame()
     {
         SceneManager.LoadSceneAsync("Game-Scene", LoadSceneMode.Single);
@@ -21,5 +39,29 @@ public class MainMenuInterface : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void DisplayMenuOptions()
+    {
+        play_ac.gameObject.SetActive(true);
+        play_ac.SetTrigger("Enter");
+        settings_ac.gameObject.SetActive(true);
+        settings_ac.SetTrigger("Enter");
+        quit_ac.gameObject.SetActive(true);
+        quit_ac.SetTrigger("Enter");
+        menu_options_background.SetActive(true);
+    }
+
+    public void HideMenuOptions()
+    {
+        logo_ac.SetTrigger("Normal");
+
+        play_ac.gameObject.SetActive(false);
+        play_ac.SetTrigger("Exit");
+        settings_ac.gameObject.SetActive(false);
+        settings_ac.SetTrigger("Exit");
+        quit_ac.gameObject.SetActive(false);
+        quit_ac.SetTrigger("Exit");
+        menu_options_background.SetActive(false);
     }
 }
