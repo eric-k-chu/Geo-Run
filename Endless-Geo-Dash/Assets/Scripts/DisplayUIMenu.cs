f9a510c2c5f52f252c519405f930c4d353e0ee27
ericchu1329 <kiyuen88ec@gmail.com>
2022-03-14 11:48:35 -0700

    Added about menu and credits menu show and hide
    - added generic UI show and hide animator for other UI displays

---
+++ Endless-Geo-Dash/Assets/Scripts/DisplayUIMenu.cs
@@ -0,0 +1,30 @@
+/*
+NAME: GPEJ
+MEMBERS: Eric Chu, Jake Wong
+COURSE: CPSC 254-01
+
+FILE DESCRIPTION:
+This file contains the DisplayUIMenu class, which contains
+functions animate the different UI menus
+*/
+using UnityEngine;
+
+public class DisplayUIMenu : MonoBehaviour
+{
+    private Animator ui_animator;
+
+    private void Awake()
+    {
+        ui_animator = GetComponent<Animator>();
+    }
+
+    public void ShowUI()
+    {
+        ui_animator.SetTrigger("Active");
+    }
+
+    public void HideUI()
+    {
+        ui_animator.SetTrigger("Inactive");
+    }
+}
